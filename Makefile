903d44e2bde09efabe63fb2e5858beac574d3f17
freilinger <freilinger@komro.net>
2021-07-20 13:08:55 +0200

    initial from demo project

---
+++ Makefile
@@ -0,0 +1,14 @@
+build:
+	cd web && docker build --no-cache -t compose-flask .
+
+compose: build
+	docker-compose up
+
+stop:
+	docker-compose down
+
+update:
+	/bin/bash copy2container.sh
+
+buhu:
+	echo "test"
---
+++ web/app.py
@@ -0,0 +1,49 @@
+# Import needed modules
+from flask import Flask, render_template
+import requests
+import sys
+
+# Create a Flask constructor. It takes name of the current module as the argument
+app = Flask(__name__)
+
+# Base urls for the cocktails api
+api_url = 'https://www.thecocktaildb.com/api/json/v1/1/'
+
+
+# Helper function to log something to console an view it with docker logs
+def console_log(text):
+    print(text, file=sys.stderr)
+
+
+# Create a route decorator to tell the application,
+# which URL should be called for the #described function and define the function
+
+
+@app.route('/')
+def start_page():
+
+    return render_template('start_page.html')
+
+
+@app.route('/cocktails_by_letter/<letter>')
+def cocktails_by_letter(letter):
+
+    url = api_url + 'search.php?f=' + letter
+    cocktails = requests.get(url)
+
+    return render_template('drinks.html', cocktails=cocktails.json())
+
+
+@app.route('/cocktail/<iid>')
+def cocktail_by_id(iid):
+
+    url = api_url + 'lookup.php?i=' + iid
+    cocktails = requests.get(url).json()
+
+    return render_template('drink-details.html', cocktail=cocktails['drinks'][0])
+
+
+# Create the main driver function
+if __name__ == '__main__':
+    # call the run method
+    app.run(host='0.0.0.0')
